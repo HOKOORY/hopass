4b37130fba4ebbbca5c9007e78c86a2c36aea6de
hokoory <634085832@qq.com>
2020-08-27 10:58:24 +0800

    tokenserver完成，并全部使用xml配置sql

--- src/main/java/com/hokoory/hopass/pass/service/impl/UserServiceImpl.java
+++ src/main/java/com/hokoory/hopass/pass/service/impl/UserServiceImpl.java
@@ -1,10 +1,14 @@
 package com.hokoory.hopass.pass.service.impl;
 
 import com.hokoory.hopass.pass.entity.User;
+import com.hokoory.hopass.pass.entity.UserToken;
 import com.hokoory.hopass.pass.mapper.UserMapper;
 import com.hokoory.hopass.pass.service.IUserService;
 import com.baomidou.mybatisplus.extension.service.impl.ServiceImpl;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
+
+import java.util.Map;
 
 /**
  * <p>
@@ -17,4 +21,25 @@ import org.springframework.stereotype.Service;
 @Service
 public class UserServiceImpl extends ServiceImpl<UserMapper, User> implements IUserService {
 
+    @Autowired
+    UserMapper userMapper;
+    @Override
+    public UserToken getUserByUsername(Map<String, String> map) {
+        return userMapper.getUserByUsername(map);
+    }
+
+    @Override
+    public int updateUserBySignUp(User user) {
+        return userMapper.updateUserBySignUp(user);
+    }
+
+    @Override
+    public int insertUserBySignUp(User user) {
+        return userMapper.insertUserBySignUp(user);
+    }
+
+    @Override
+    public User getUserInPass(Map<String, String> map) {
+        return userMapper.getUserInPass(map);
+    }
 }
