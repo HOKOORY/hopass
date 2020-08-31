a8e4269f13ad07d95d7439461f6d3cb26ac68cd1
hokoory <634085832@qq.com>
2020-08-31 17:20:59 +0800

    拦截器测试

--- src/main/java/com/hokoory/hopass/HopassApplication.java
+++ src/main/java/com/hokoory/hopass/HopassApplication.java
@@ -2,10 +2,12 @@ package com.hokoory.hopass;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.ImportResource;
 import org.springframework.scheduling.annotation.EnableScheduling;
 
 @SpringBootApplication
 @EnableScheduling
+@ImportResource(locations = {"classpath:web.xml"})
 public class HopassApplication {
 
     public static void main(String[] args) {
