e265bee9e5a47cbd8b25694fdd6c327b16df3afd
hokoory <HEjiale5201314>
2020-09-26 18:13:21 +0800

    新增对ActiveMq的支持

--- src/main/java/com/hokoory/hopass/HopassApplication.java
+++ src/main/java/com/hokoory/hopass/HopassApplication.java
@@ -3,11 +3,13 @@ package com.hokoory.hopass;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.context.annotation.ImportResource;
+import org.springframework.jms.annotation.EnableJms;
 import org.springframework.scheduling.annotation.EnableScheduling;
 
 @SpringBootApplication
 @EnableScheduling
 @ImportResource(locations = {"classpath:web.xml"})
+@EnableJms
 public class HopassApplication {
 
     public static void main(String[] args) {
--- src/main/java/com/hokoory/hopass/pass/controller/Test.java
+++ src/main/java/com/hokoory/hopass/pass/controller/Test.java
@@ -1,79 +1,56 @@
 package com.hokoory.hopass.pass.controller;
 
+import com.baomidou.mybatisplus.core.conditions.query.QueryWrapper;
 import com.hokoory.hopass.pass.entity.Response;
 import com.hokoory.hopass.pass.entity.User;
 import com.hokoory.hopass.pass.mapper.UserMapper;
+import com.hokoory.hopass.utils.XORUtils;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.data.redis.core.RedisTemplate;
 import org.springframework.web.bind.annotation.*;
 
+import javax.jms.Queue;
+import javax.jms.Topic;
 import java.io.Serializable;
 import java.util.HashMap;
 import java.util.Map;
 
 
 @RestController
-public class Test{
+public class Test {
     @Autowired
     UserMapper userMapper;
     @Autowired
     RedisTemplate<String, Serializable> redisTemplate;
     @Autowired
     RedisTemplate<String, Object> objectRedisTemplate;
+    @Autowired
+    private Queue queue;
+    @Autowired
+    private Topic topic;
 
     @RequestMapping(value = "/test")
     public Object test() {
-
-//        int x = 71;
-//        int[] arr = {1,2,3,4};
-//        for(int i = 0;i<arr.length;i++){
-//            if(x == arr[i]){
-//                return i;
-//            }
-//        }
-//        return -1;
-
-
-//        int sum=0;
-//        int n[] = {1,2,3,4,5};
-//        System.out.println(n.length);
-//        for(int i=1;i<n.length;i++){
-//            sum = sum+n[i];
-//        }
-//        System.out.println("sum="+sum);
-//        return null;
-
-//        String name = "";
-//        String password = "";
-//        try{
-//            List<User> list = findBySQL("select * from user where phone = ? or username = ?",name,name);
-//            if (list.size() > 0){
-//                for (User user : list) {
-//                    if (user.getpassword == md5(password)){
-//                        return 1;
-//                    }
-//                }
-//                return -2;
-//            }else {
-//                return -1;
-//            }
-//        }catch (Exception e){
-//          return  -3;
-//        }
-
-
-
-
+        /*
+        //测试一对多
         Map<String, String> map = new HashMap<>();
         map.put("id", "17");
         User user = userMapper.getUserInPass(map);
         return new Response<>(user);
+        */
 
-//        String key = "user:1";
-//        User user = userMapper.selectOne(new QueryWrapper<User>().eq("user_name", "admin"));
-//        objectRedisTempl ate.opsForValue().set(key, user);
-//        User user1 = (User) objectRedisTemplate.opsForValue().get(key);
-//        return XORUtils.decrypt(user1.getKeygen().getBytes(), (user1.getId() + user1.getUserName()).getBytes());
-//        User user = userMapper.selectOne(new QueryWrapper<User>().eq("user_name","admin"));
+        /*
+        //测试redis
+        //测试 XOR加解密
+        String key = "user:1";
+        Map<String, String> map = new HashMap<>();
+        map.put("user_name", "admin");
+        User user = userMapper.getUserByUsername(map);
+        objectRedisTemplate.opsForValue().set(key, user);
+        User user1 = (User) objectRedisTemplate.opsForValue().get(key);
+        String XORDecode = new String(XORUtils.decrypt(user1.getKeygen().getBytes(), (user1.getId() + user1.getUserName()).getBytes()));
+        return XORDecode;
+        */
+        return null;
     }
 }
