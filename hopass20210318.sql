f348d68459a99c789d9dfb4705f089ef8b813723
hokoory <634085832@qq.com>
2021-07-16 11:17:15 +0800

    SQL文件备份

---
+++ hopass20210318.sql
@@ -0,0 +1,85 @@
+/*
+ Navicat Premium Data Transfer
+
+ Source Server         : 本地
+ Source Server Type    : MySQL
+ Source Server Version : 50726
+ Source Host           : localhost:3306
+ Source Schema         : hopass
+
+ Target Server Type    : MySQL
+ Target Server Version : 50726
+ File Encoding         : 65001
+
+ Date: 18/03/2021 17:27:39
+*/
+
+SET NAMES utf8mb4;
+SET FOREIGN_KEY_CHECKS = 0;
+
+-- ----------------------------
+-- Table structure for config
+-- ----------------------------
+DROP TABLE IF EXISTS `config`;
+CREATE TABLE `config`  (
+  `id` int(11) UNSIGNED NOT NULL AUTO_INCREMENT,
+  `config_key` varchar(255) CHARACTER SET utf8mb4 COLLATE utf8mb4_general_ci NULL DEFAULT NULL COMMENT 'key',
+  `config_value` text CHARACTER SET utf8mb4 COLLATE utf8mb4_general_ci NULL COMMENT 'value',
+  `introduction` varchar(255) CHARACTER SET utf8mb4 COLLATE utf8mb4_general_ci NULL DEFAULT NULL COMMENT '简介',
+  `create_time` int(11) NULL DEFAULT NULL COMMENT '创建时间',
+  PRIMARY KEY (`id`) USING BTREE
+) ENGINE = InnoDB AUTO_INCREMENT = 2 CHARACTER SET = utf8mb4 COLLATE = utf8mb4_general_ci ROW_FORMAT = DYNAMIC;
+
+-- ----------------------------
+-- Records of config
+-- ----------------------------
+INSERT INTO `config` VALUES (1, 'cansignup', '1', '能否注册', 213);
+
+-- ----------------------------
+-- Table structure for password
+-- ----------------------------
+DROP TABLE IF EXISTS `password`;
+CREATE TABLE `password`  (
+  `id` int(11) NOT NULL AUTO_INCREMENT,
+  `user_id` int(11) NULL DEFAULT NULL COMMENT '所属用户ID',
+  `title` varchar(255) CHARACTER SET utf8mb4 COLLATE utf8mb4_general_ci NULL DEFAULT NULL COMMENT '标题',
+  `context` varchar(255) CHARACTER SET utf8mb4 COLLATE utf8mb4_general_ci NULL DEFAULT NULL COMMENT '内容',
+  `web` varchar(255) CHARACTER SET utf8mb4 COLLATE utf8mb4_general_ci NULL DEFAULT NULL COMMENT '密码对应那个网站',
+  `account` varchar(255) CHARACTER SET utf8mb4 COLLATE utf8mb4_general_ci NULL DEFAULT NULL COMMENT '账号',
+  `password` varchar(255) CHARACTER SET utf8mb4 COLLATE utf8mb4_general_ci NULL DEFAULT NULL COMMENT '密码  密码通过自有加密算法',
+  `create_time` int(11) NULL DEFAULT NULL COMMENT '创建时间',
+  `update_time` int(11) NULL DEFAULT NULL COMMENT '修改时间',
+  PRIMARY KEY (`id`) USING BTREE,
+  INDEX `user`(`user_id`) USING BTREE,
+  CONSTRAINT `user` FOREIGN KEY (`user_id`) REFERENCES `user` (`id`) ON DELETE RESTRICT ON UPDATE RESTRICT
+) ENGINE = InnoDB AUTO_INCREMENT = 3 CHARACTER SET = utf8mb4 COLLATE = utf8mb4_general_ci ROW_FORMAT = DYNAMIC;
+
+-- ----------------------------
+-- Records of password
+-- ----------------------------
+INSERT INTO `password` VALUES (1, 17, '231', '123', '123', '123', '123', 123, NULL);
+INSERT INTO `password` VALUES (2, 17, '123', '44444', '11111', '14141414', '41415566', 123, NULL);
+
+-- ----------------------------
+-- Table structure for user
+-- ----------------------------
+DROP TABLE IF EXISTS `user`;
+CREATE TABLE `user`  (
+  `id` int(11) NOT NULL AUTO_INCREMENT,
+  `user_name` varchar(255) CHARACTER SET utf8mb4 COLLATE utf8mb4_general_ci NULL DEFAULT NULL COMMENT '用户名',
+  `password` varchar(255) CHARACTER SET utf8mb4 COLLATE utf8mb4_general_ci NULL DEFAULT NULL COMMENT '密码',
+  `keygen` varchar(255) CHARACTER SET utf8mb4 COLLATE utf8mb4_general_ci NULL DEFAULT NULL COMMENT 'aes 的key 通过加密 后存在此字段',
+  `salt` varchar(255) CHARACTER SET utf8mb4 COLLATE utf8mb4_general_ci NULL DEFAULT NULL COMMENT '盐',
+  `ban_time` int(11) NULL DEFAULT 0 COMMENT '空为没封号',
+  `create_time` int(11) NULL DEFAULT NULL,
+  PRIMARY KEY (`id`) USING BTREE
+) ENGINE = InnoDB AUTO_INCREMENT = 45 CHARACTER SET = utf8mb4 COLLATE = utf8mb4_general_ci ROW_FORMAT = DYNAMIC;
+
+-- ----------------------------
+-- Records of user
+-- ----------------------------
+INSERT INTO `user` VALUES (17, 'admin', '3db41699a848275f8469b540cb32c3c1', 'GxMoLBEVbUE/JlNXDAxbHikDbTM4JUAaBCEwJyUMXx8RKF4qCjYUBT05dwMBHV0kJFUcFCM2bxADJDc7', '9BRY', 0, 1597994503);
+INSERT INTO `user` VALUES (18, 'admin1', 'f11dcb6fb0e96ae1bdae1b49de3c2e66', 'FGYsITsUfRswJD9CFw4eHys9Ry0kFCNkAggCAB9oQB0bIQ92CycEJic7KgEfEzlrHjMyLjxCcyoUF087', 'wl5r', 0, 1597994517);
+INSERT INTO `user` VALUES (44, 'admin12', '1b9f5f77dec5c90a1249073182d3f7cf', 'Gj8SLT8XAkElNAU2GB0WCB07TzslFT8lNG4CAicNZRYMEwsMJyU2ASA7YyovdBsjMx8xajkqXQU+KU9/', '4SRR', 0, 1598424523);
+
+SET FOREIGN_KEY_CHECKS = 1;
